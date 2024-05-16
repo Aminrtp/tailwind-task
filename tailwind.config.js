26e75ca1b31595354918871e77366dcc98d1fdbd
amin <aminrahmatpoor7@gmail.com>
2024-05-17 01:03:53 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily:{
+          sans:["vazir"]
+        }
+      },
+    },
+    plugins: [
+      require("@tailwindcss/forms")({
+        strategy:"class",
+      }),
+    ],
+  }
